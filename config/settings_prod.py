c30be4f9fdd99db6a72d886dd21748d316ebf5d0
Jaume <jnualart@uoc.edu>
2026-08-20 17:30:33 +0200

    Add production settings

---
+++ config/settings_prod.py
@@ -0,0 +1,34 @@
+import os
+
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+ALLOWED_HOSTS = [
+    "ai.selectora.cc",
+    "127.0.0.1",
+    "localhost",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ai.selectora.cc",
+]
+
+DATABASES["default"]["NAME"] = (
+    BASE_DIR.parent / "data" / "db.sqlite3"
+)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.parent / "media"
+
+SECURE_PROXY_SSL_HEADER = (
+    "HTTP_X_FORWARDED_PROTO",
+    "https",
+)
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
